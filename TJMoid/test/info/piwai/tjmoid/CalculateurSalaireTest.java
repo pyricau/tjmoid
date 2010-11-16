f9988441704e11fea803fb01229741a78ea8cd75
py.ricau <py.ricau@71815c10-68fb-2bc7-54aa-f52b5fa096ec>
2010-11-16 09:51:42 +0000

    Refactoring, + added tests

--- TJMoid/test/info/piwai/tjmoid/CalculateurSalaireTest.java
+++ TJMoid/test/info/piwai/tjmoid/CalculateurSalaireTest.java
@@ -14,15 +14,23 @@ public class CalculateurSalaireTest {
 		builder.salaireBrutDeBase(2584) //
 				.tauxChargesSocialesPatronales(1.58) //
 				.tauxPartageIngeEntreprise(0.6) //
-				.tauxMargeCommerciale(0.1) //
 				.nbJoursTravaillesAnnuels(219);
+		
+		
 
-		CalculateurSalaire calculateurSalaire = builder.build();
+		CalculateurSalaire calculateurSalaire10Pourcent = builder.tauxMargeCommerciale(0.1).build();
 
-		assertEquals(37424, calculateurSalaire.calculerSalaireBrut(500));
-		assertEquals(31008, calculateurSalaire.calculerSalaireBrut(350));
-		assertEquals(31008, calculateurSalaire.calculerSalaireBrut(400));
-		assertEquals(59878, calculateurSalaire.calculerSalaireBrut(800));
+		assertEquals(31008, calculateurSalaire10Pourcent.calculerSalaireBrut(350));
+		assertEquals(31008, calculateurSalaire10Pourcent.calculerSalaireBrut(400));
+		assertEquals(37424, calculateurSalaire10Pourcent.calculerSalaireBrut(500));
+		assertEquals(59878, calculateurSalaire10Pourcent.calculerSalaireBrut(800));
+		
+		CalculateurSalaire calculateurSalaire05Pourcent = builder.tauxMargeCommerciale(0.05).build();
+		
+		assertEquals(31008, calculateurSalaire05Pourcent.calculerSalaireBrut(350));
+		assertEquals(31603, calculateurSalaire05Pourcent.calculerSalaireBrut(400));
+		assertEquals(39503, calculateurSalaire05Pourcent.calculerSalaireBrut(500));
+		assertEquals(63205, calculateurSalaire05Pourcent.calculerSalaireBrut(800));
 
 	}
 
