174f50d7a6a262c7fc653a8421855c16dd7746b8
py.ricau <py.ricau@71815c10-68fb-2bc7-54aa-f52b5fa096ec>
2010-11-16 06:44:54 +0000

    Sharing initial project

---
+++ TJMoid/src/info/piwai/tjmoid/TJMoid.java
@@ -0,0 +1,13 @@
+package info.piwai.tjmoid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TJMoid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
