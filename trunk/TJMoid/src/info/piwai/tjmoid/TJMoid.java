83964b9434dd4f5f032cac42ef7fef5d9f2ea8a2
py.ricau <py.ricau@71815c10-68fb-2bc7-54aa-f52b5fa096ec>
2010-11-16 11:17:37 +0000

    Implem initiale

--- trunk/TJMoid/src/info/piwai/tjmoid/TJMoid.java
+++ trunk/TJMoid/src/info/piwai/tjmoid/TJMoid.java
@@ -1,13 +1,63 @@
 package info.piwai.tjmoid;
 
+import info.piwai.tjmoid.CalculateurSalaire.Builder;
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class TJMoid extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	private EditText tjmInput;
+	private TextView salaireTextView;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		tjmInput = (EditText) findViewById(R.id.tjmInput);
+		salaireTextView = (TextView) findViewById(R.id.salaireTextView);
+
+		tjmInput.addTextChangedListener(new AbstractTextWatcher() {
+			@Override
+			public void afterTextChanged(Editable s) {
+
+				String tjmString = s.toString();
+				long tjm;
+				if ("".equals(tjmString)) {
+					tjm = 0;
+				} else {
+					tjm = Long.parseLong(tjmString);
+				}
+				onTjmChanged(tjm);
+			}
+		});
+
+	}
+
+	private void onTjmChanged(long tjm) {
+
+		CalculateurSalaire calculateurSalaire = buildCalculateurSalaire();
+
+		long salaireBrut = calculateurSalaire.calculerSalaireBrut(tjm);
+
+		salaireTextView.setText(salaireBrut + " euros bruts annuels");
+	}
+
+	private CalculateurSalaire buildCalculateurSalaire() {
+		Builder builder = new CalculateurSalaire.Builder();
+		builder.salaireBrutDeBase(2584) //
+				.tauxChargesSocialesPatronales(1.58) //
+				.tauxPartageIngeEntreprise(0.6) //
+				.nbJoursTravaillesAnnuels(219) //
+				.tauxMargeCommerciale(0.1);
+		CalculateurSalaire calculateurSalaire = builder.build();
+		return calculateurSalaire;
+	}
+
 }
