a844336651d24525586098d22bd6a584dfa70f4e
py.ricau <py.ricau@71815c10-68fb-2bc7-54aa-f52b5fa096ec>
2010-11-16 21:17:25 +0000

    une version avec des louarne facts... cachée

--- TJMoid/src/info/piwai/tjmoid/TJMoid.java
+++ TJMoid/src/info/piwai/tjmoid/TJMoid.java
@@ -3,16 +3,32 @@ package info.piwai.tjmoid;
 import info.piwai.tjmoid.CalculateurSalaire.Builder;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
-import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class TJMoid extends Activity {
+	
+	private class LouarneFactRunnable implements Runnable {
+		public void run() {
+			showLouarneFact();
+		}
+	}
 
 	private EditText tjmInput;
 	private TextView salaireTextView;
+
+	private final LouarneFactGenerator louarneFactGenerator = new LouarneFactGenerator();
+	
+	private final Handler handler = new Handler();
+	
+	private boolean paused = true;
+	
+	private final LouarneFactRunnable louarneFactRunnable = new LouarneFactRunnable();
+	
+
 
 	/** Called when the activity is first created. */
 	@Override
@@ -58,6 +74,31 @@ public class TJMoid extends Activity {
 				.tauxMargeCommerciale(0.1);
 		CalculateurSalaire calculateurSalaire = builder.build();
 		return calculateurSalaire;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		paused = false;
+		showLouarneFact();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		paused = true;
+	}
+
+	private void showLouarneFact() {
+		if (!paused) {
+			String randomLouarneFact = louarneFactGenerator.getRandomLouarneFact();
+			Toast.makeText(this, randomLouarneFact, Toast.LENGTH_LONG).show();
+			planNextLouarneFact();
+		}
+	}
+
+	private void planNextLouarneFact() {
+		handler.postDelayed(louarneFactRunnable, 5000);
 	}
 
 }
