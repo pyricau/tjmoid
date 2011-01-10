46a94b6655f59242b22528a6a690c0983e9e99cd
py.ricau <py.ricau@71815c10-68fb-2bc7-54aa-f52b5fa096ec>
2011-01-10 19:05:37 +0000

    Tag TJMoid 1.7

---
+++ tags/TJMoid 1.7/TJMoid/src/info/piwai/tjmoid/Constants.java
@@ -0,0 +1,11 @@
+package info.piwai.tjmoid;
+
+public abstract class Constants {
+	
+	private Constants() {}
+	
+	public static final String VERSION_NAME = "1.7";
+	public static final int VERSION = 8;
+	public static final String ANALYTICS_ID = "UA-10826408-5";
+
+}
