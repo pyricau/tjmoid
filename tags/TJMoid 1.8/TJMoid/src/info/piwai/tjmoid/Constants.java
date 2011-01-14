c76c9426522c06ed2e9ee93bcf96fc7a61ad6362
py.ricau <py.ricau@71815c10-68fb-2bc7-54aa-f52b5fa096ec>
2011-01-14 07:23:18 +0000

    tag v 1.8

---
+++ tags/TJMoid 1.8/TJMoid/src/info/piwai/tjmoid/Constants.java
@@ -0,0 +1,9 @@
+package info.piwai.tjmoid;
+
+public abstract class Constants {
+	
+	private Constants() {}
+	
+	public static final String ANALYTICS_ID = "UA-10826408-5";
+
+}
---
+++ tags/TJMoid 1.8/TJMoid/src/info/piwai/tjmoid/TrackingPreferenceActivity.java
@@ -0,0 +1,57 @@
+package info.piwai.tjmoid;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+import com.google.android.apps.analytics.GoogleAnalyticsTracker;
+
+public abstract class TrackingPreferenceActivity extends PreferenceActivity {
+
+	private GoogleAnalyticsTracker tracker;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		tracker = GoogleAnalyticsTracker.getInstance();
+
+		tracker.start(Constants.ANALYTICS_ID, 20, this);
+		
+		new Thread() {
+			@Override
+			public void run() {
+				String versionName;
+				String versionCode;
+				try {
+					PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+					versionName = packageInfo.versionName;
+					versionCode = String.valueOf(packageInfo.versionCode);
+				} catch (NameNotFoundException e) {
+					versionName = "UNKNOWN";
+					versionCode = "0";
+				}
+
+				tracker.setCustomVar(1, "versionName", versionName, 2);
+				tracker.setCustomVar(2, "versionCode", versionCode, 2);
+
+				tracker.setCustomVar(3, "release", Build.VERSION.RELEASE, 2);
+				tracker.setCustomVar(4, "model", Build.MODEL, 2);
+
+				tracker.trackPageView("/" + getClass().getSimpleName());
+			}
+		}.start();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		tracker.stop();
+	}
+
+	public GoogleAnalyticsTracker getTracker() {
+		return tracker;
+	}
+
+}
